691f147cd430cb262468ff1d3c1da4beace66784
jsomville <jsomville@hotmail.com>
2021-07-05 20:41:19 +0200

    Add Scene with menu embedded

---
+++ SceneMenu.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import pygame
+import random
+
+from Scene import Scene
+from Colors import Colors
+from Util import Util
+from MenuHOrientation import MenuHOrientation
+from MenuVOrientation import MenuVOrientation
+from MenuTheme import MenuTheme
+from MenuUtil import MenuUtil
+from MenuObjectType import MenuObjectType
+
+class SceneMenu(Scene):
+    
+    def on_init(self):
+        """ initialize menu objects """
+        self.menu_list = list()
+        self.menu_dict = dict()
+
+    def addLabel(self, name, text):
+        """ add a label in menu list not in menu_dict """
+        btn = dict()
+        btn["name"] = name
+        btn["type"] = MenuObjectType.TITLE
+        btn["text"] = text
+        self.menu_list.append(btn)
+
+    def addMenu(self, name, text, goto):
+        """ add a button in menu list and in menu_dict """
+        btn = dict()
+        btn["name"] = name
+        btn["type"] = MenuObjectType.BUTTON
+        btn["text"] = text
+        btn["goto"] = goto
+        self.menu_list.append(btn)
+        self.menu_dict[btn["name"]] = btn
+
+    def on_event(self, event):
+        """ event for menu specific """
+
+        #Handle menu specific events (Hover, Click)
+        self.menu_util.on_event(event, self.menu_list)
+
+        #Handle action specified in menu
+        for control in self.menu_list:
+            if control["type"] == MenuObjectType.BUTTON:
+                if "click" in control:
+                    if control["click"]:
+                        if control["name"] == "btnQuit":
+                            self.next = None
+                        else:
+                            if control["name"] in self.menu_dict:
+                                self.fire_goto_event(control["goto"])
+
+                        #Clear Click Event
+                        control["click"] = False          
+       
+       
+    def on_render(self, surface):
+        """ Renders the menu """
+        #Draw Menu
+        self.menu_util.draw_menu(surface, self.menu_list)
--- example_SceneMenu.py
+++ example_SceneMenu.py
@@ -2,7 +2,7 @@
 import pygame
 import random
 
-from Scene import Scene
+from SceneMenu import SceneMenu
 from Colors import Colors
 from Util import Util
 from MenuHOrientation import MenuHOrientation
@@ -11,7 +11,7 @@ from MenuTheme import MenuTheme
 from MenuUtil import MenuUtil
 from MenuObjectType import MenuObjectType
 
-class Scene_Menu(Scene):
+class Scene_Menu(SceneMenu):
     
     def on_init(self):
         self.name = "menu"
@@ -25,10 +25,9 @@ class Scene_Menu(Scene):
         self.title_font = pygame.font.SysFont("Arial", 24)
 
         #Menu Init
+        SceneMenu.on_init(self)
         self.v_orientation = MenuVOrientation.CENTER
         self.h_orientation = MenuHOrientation.CENTER
-        self.menu_list = list()
-        self.menu_dict = dict()
         self.theme = MenuTheme()
         self.menu_util = MenuUtil()
 
@@ -38,48 +37,16 @@ class Scene_Menu(Scene):
 
         self.addMenu("btn1", "Click Rectangle", "Scene 1")
 
-        self.addMenu("btnQuit", "Quit", None)
+        self.addMenu("btnQuit", "Quit", None) # btnQuit : This is a reserved word
 
         #****************************************
 
         #Last step of intitialisation
         self.menu_util.on_init(self.size, self.menu_list, self.v_orientation, self.h_orientation)
         self.inited = True
-   
-    def addLabel(self, name, text):
-        btn = dict()
-        btn["name"] = name
-        btn["type"] = MenuObjectType.TITLE
-        btn["text"] = text
-        self.menu_list.append(btn)
-
-    def addMenu(self, name, text, goto):
-        btn = dict()
-        btn["name"] = name
-        btn["type"] = MenuObjectType.BUTTON
-        btn["text"] = text
-        btn["goto"] = goto
-        self.menu_list.append(btn)
-        self.menu_dict[btn["name"]] = btn
 
     def on_event(self, event):
-
-        #Handle menu specific events
-        self.menu_util.on_event(event, self.menu_list)
-
-        for control in self.menu_list:
-            if control["type"] == MenuObjectType.BUTTON:
-                if "click" in control:
-                    if control["click"]:
-                        if control["name"] == "btnQuit":
-                            self.next = None
-                        else:
-                            if control["name"] in self.menu_dict:
-                                self.fire_goto_event(control["goto"])
-
-                        #Clear Click Event
-                        control["click"] = False          
-
+        SceneMenu.on_event(self, event)
 
     def on_loop(self):
         pass
@@ -87,8 +54,7 @@ class Scene_Menu(Scene):
     def on_render(self, surface):
         surface.fill(self.BACKGROUND)
 
-        #Draw Menu
-        self.menu_util.draw_menu(surface, self.menu_list)
+        SceneMenu.on_render(self, surface)
 
         #Draw Title on center surface x
         Util().draw_text_center_x(surface, self.title, self.title_font, Colors.BLACK, 15)
